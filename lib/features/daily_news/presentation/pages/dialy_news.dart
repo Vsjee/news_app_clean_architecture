25783c196675ad475962cbf4590c6132584c9c5a
David Hernandez <davidfelipehernandez1@gmail.com>
2023-10-26 10:34:58 -0500

    add page dialyNews

---
+++ lib/features/daily_news/presentation/pages/dialy_news.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+class DialyNews extends StatelessWidget {
+  const DialyNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppbar(),
+      body: _buildBody(),
+    );
+  }
+
+  _buildAppbar() {
+    return AppBar(
+      title: const Text(
+        'Dialy News',
+        style: TextStyle(color: Colors.black, fontSize: 25),
+      ),
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticlesBloc, RemoteArticleState>(
+      builder: (_, state) {
+        if (state is RemoteArticlesLoading) {
+          return const Center(child: CupertinoActivityIndicator());
+        }
+
+        if (state is RemoteArticlesError) {
+          return const Center(child: Icon(Icons.refresh));
+        }
+
+        if (state is RemoteArticlesDone) {
+          return ListView.builder(
+            itemBuilder: (ctx, idx) {
+              return ListTile(
+                title: Text('$idx'),
+              );
+            },
+            itemCount: state.articles!.length,
+          );
+        }
+
+        return const SizedBox();
+      },
+    );
+  }
+}
